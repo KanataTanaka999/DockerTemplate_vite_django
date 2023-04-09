f669e05a297fc626f916d8e99cc4ccce3e2c13f8
KanataTanaka999 <lull.silver9@gmail.com>
2023-04-09 19:30:33 +0900

    commit

--- front/app/vite.config.ts
+++ front/app/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+  ],
 })
